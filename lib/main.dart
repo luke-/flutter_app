d857cd1345003c97fd2b6c28ad57ddfb327de63d
Lucas Bartholemy <lucas@bartholemy.com>
2023-01-12 11:47:03 +0100

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'Pages/Login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xff21A1B3),
+      ),
+      home: LoginDemo(),
+    );
+  }
+}
